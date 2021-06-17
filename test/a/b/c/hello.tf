b290ab862e4f75d10c39debca84bb5db574628ee
Dan Amzulescu <35110114+qualidan@users.noreply.github.com>
2021-06-17 16:49:49 -0500

    Update hello.tf

--- test/a/b/c/hello.tf
+++ test/a/b/c/hello.tf
@@ -5,7 +5,11 @@ terraform {
   required_version = ">= 0.12.26"
 }
 
-# website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
-output "hello_world" {
-  value = "Hello, World!"
+output "hello" {
+ value = var.hello
+}
+
+variable "hello" {
+ type = string
+ default = "World!"
 }
