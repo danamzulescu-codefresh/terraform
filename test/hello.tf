8e8a94fe890d110f6793f3d0007f86bd48018829
Dan Amzulescu <97133273+danamzulescu-codefresh@users.noreply.github.com>
2022-04-06 09:07:27 -0500

    Update hello.tf

--- test/hello.tf
+++ test/hello.tf
@@ -6,6 +6,6 @@ terraform {
 }
 
 # website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
-output "hello_world" {
-  value = "Hello, World!"
+output 'hello_world' {
+  value = 'Hello, World!'
 }
