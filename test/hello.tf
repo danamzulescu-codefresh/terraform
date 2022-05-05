f34ab877a7b81f5cf48ee7b94a89416ccf576518
Dan Amzulescu <dan.amzulescu@codefresh.io>
2022-05-05 11:26:37 -0500

    0

--- test/hello.tf
+++ test/hello.tf
@@ -7,5 +7,5 @@ terraform {
 
 # website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
 output "hello_world" {
-  value = "Hello, World!8"
+  value = "Hello, World!0"
 }
