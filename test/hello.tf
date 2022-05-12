0bdf7283c6fce6d9ef6de26dc031d1e8e542b4a7
Dan Amzulescu <dan.amzulescu@codefresh.io>
2022-05-12 11:58:57 -0500

    Update hello.tf

--- test/hello.tf
+++ test/hello.tf
@@ -5,7 +5,7 @@ terraform {
   required_version = ">= 0.12.26"
 }
 
-# website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
+# website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World3!"
 output "hello_world" {
   value = "Hello, World!"
 }
